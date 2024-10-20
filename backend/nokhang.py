62eb6e863dbdfecbb701d6e84fd69c7520361b6e
BlueTot <bluetotkc@gmail.com>
2024-10-20 17:53:00 +0100

    added function

--- backend/nokhang.py
+++ backend/nokhang.py
@@ -1,4 +1,26 @@
-class Error(Exception):
-    pass
+def get_none():
+    return None
 
-print("hello world!")
+def is_even(num):
+    if num == 0:
+        return True
+    elif num == 1:
+        return False
+    elif num == 2:
+        return True
+    elif num == 3:
+        return False
+    elif num == 4:
+        return True
+    elif num == 5:
+        return False
+    elif num == 6:
+        return True
+    elif num == 7:
+        return False
+    elif num == 8:
+        return True
+    elif num == 9:
+        return False
+    else:
+        raise ValueError("Number out of range")
